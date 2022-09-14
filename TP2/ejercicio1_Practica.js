c4b2ef05602579ec516c98a99e40d192a3ad2f69
DamianECornejo <damiancorbit@gmail.com>
2022-09-14 17:01:21 -0300

    Practica

---
+++ TP2/ejercicio1_Practica.js
@@ -0,0 +1,20 @@
+//ejercicio 1 
+
+    var verdadero, char;
+    var valores = [verdadero, false, 2, "hola", "mundo", 3, char]
+    
+  
+
+    var valorresStr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (typeof valores[i] === "string") {
+          valores.push (valores[i])  
+        }
+        
+    }
+    console.log ("2, Imprimir los elementos de menores a mayor cantidad de letra: " + valorresStr.sort(function(a,b){
+  
+        return a-b;
+        
+      }));
+      
